6116bc2ab384545b5e1025477d6cec12390683a8
Tom <tom.ewbank.18@gmail.com>
2017-03-22 00:42:27 +0100

    single cam calibration + img  rectification

--- calibration/calib_intrinsic.cpp
+++ calibration/calib_intrinsic.cpp
@@ -3,8 +3,8 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <stdio.h>
+#include <dirent.h>
 #include <iostream>
-#include "popt_pp.h"
 
 using namespace std;
 using namespace cv;
@@ -16,110 +16,135 @@ vector< Point2f > corners;
 Mat img, gray;
 Size im_size;
 
-void setup_calibration(int board_width, int board_height, int num_imgs, 
-                       float square_size, char* imgs_directory, char* imgs_filename,
-                       char* extension) {
-  Size board_size = Size(board_width, board_height);
-  int board_n = board_width * board_height;
+void setup_calibration(int board_width, int board_height,
+                       float square_size, char* imgs_directory, bool showChessBoard) {
+    Size board_size = Size(board_width, board_height);
+    int board_n = board_width * board_height;
 
-  for (int k = 1; k <= num_imgs; k++) {
-    char img_file[100];
-    sprintf(img_file, "%s%s%d.%s", imgs_directory, imgs_filename, k, extension);
-    img = imread(img_file, CV_LOAD_IMAGE_COLOR);
-    cv::cvtColor(img, gray, CV_BGR2GRAY);
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir (imgs_directory)) != NULL) {
+        /* print all the files and directories within directory */
+        int k = 0;
+        while ((ent = readdir (dir)) != NULL) {
 
-    bool found = false;
-    found = cv::findChessboardCorners(img, board_size, corners,
-                                      CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FILTER_QUADS);
-    if (found)
-    {
-      cornerSubPix(gray, corners, cv::Size(5, 5), cv::Size(-1, -1),
-                   TermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.1));
-      drawChessboardCorners(gray, board_size, corners, found);
+            if ( !strcmp(ent->d_name, ".") || !strcmp(ent->d_name, "..") )
+            {
+                continue;
+            } else {
+                char img_file[100];
+                sprintf(img_file, "%s/%s", imgs_directory, ent->d_name);
+                cout << img_file << endl;
+                img = imread(img_file, CV_LOAD_IMAGE_COLOR);
+                cv::cvtColor(img, gray, CV_BGR2GRAY);
+
+                bool found = cv::findChessboardCorners(img, board_size, corners,
+                                                  CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FILTER_QUADS);
+                if (found && showChessBoard) {
+                    cornerSubPix(gray, corners, cv::Size(5, 5), cv::Size(-1, -1),
+                                 TermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.1));
+                    drawChessboardCorners(gray, board_size, corners, found);
+                    namedWindow("Chessboard detected");
+                    imshow("Chessboard detected", gray);
+                    waitKey();
+                }
+
+                vector<Point3f> obj;
+                for (int i = 0; i < board_height; i++)
+                    for (int j = 0; j < board_width; j++)
+                        obj.push_back(Point3f((float) j * square_size, (float) i * square_size, 0));
+
+                if (found) {
+                    cout << k << ". Found corners!" << endl;
+                    image_points.push_back(corners);
+                    object_points.push_back(obj);
+                }
+                ++k;
+            }
+        }
+        closedir (dir);
+    } else {
+        /* could not open directory */
+        cout << "Could not open directory!" << endl;
     }
-    
-    vector< Point3f > obj;
-    for (int i = 0; i < board_height; i++)
-      for (int j = 0; j < board_width; j++)
-        obj.push_back(Point3f((float)j * square_size, (float)i * square_size, 0));
-
-    if (found) {
-      cout << k << ". Found corners!" << endl;
-      image_points.push_back(corners);
-      object_points.push_back(obj);
-    }
-  }
 }
 
 double computeReprojectionErrors(const vector< vector< Point3f > >& objectPoints,
                                  const vector< vector< Point2f > >& imagePoints,
                                  const vector< Mat >& rvecs, const vector< Mat >& tvecs,
                                  const Mat& cameraMatrix , const Mat& distCoeffs) {
-  vector< Point2f > imagePoints2;
-  int i, totalPoints = 0;
-  double totalErr = 0, err;
-  vector< float > perViewErrors;
-  perViewErrors.resize(objectPoints.size());
+    vector< Point2f > imagePoints2;
+    int i, totalPoints = 0;
+    double totalErr = 0, err;
+    vector< float > perViewErrors;
+    perViewErrors.resize(objectPoints.size());
 
-  for (i = 0; i < (int)objectPoints.size(); ++i) {
-    projectPoints(Mat(objectPoints[i]), rvecs[i], tvecs[i], cameraMatrix,
-                  distCoeffs, imagePoints2);
-    err = norm(Mat(imagePoints[i]), Mat(imagePoints2), CV_L2);
-    int n = (int)objectPoints[i].size();
-    perViewErrors[i] = (float) std::sqrt(err*err/n);
-    totalErr += err*err;
-    totalPoints += n;
-  }
-  return std::sqrt(totalErr/totalPoints);
+    for (i = 0; i < (int)objectPoints.size(); ++i) {
+        projectPoints(Mat(objectPoints[i]), rvecs[i], tvecs[i], cameraMatrix,
+                      distCoeffs, imagePoints2);
+        err = norm(Mat(imagePoints[i]), Mat(imagePoints2), CV_L2);
+        int n = (int)objectPoints[i].size();
+        perViewErrors[i] = (float) std::sqrt(err*err/n);
+        totalErr += err*err;
+        totalPoints += n;
+    }
+    return std::sqrt(totalErr/totalPoints);
 }
 
 int main(int argc, char const **argv)
 {
-  int board_width, board_height, num_imgs;
-  float square_size;
-  char* imgs_directory;
-  char* imgs_filename;
-  char* out_file;
-  char* extension;
+    char imgs_directory[100];
+    sprintf(imgs_directory, "calib_imgs/15_495mm");
+    int board_width = 17;
+    int board_height = 11;
+    float square_size = 0.015495;//0.02423;
+    string out_file = "camCalib.yml";
+    bool showChessboard = true;
 
-  static struct poptOption options[] = {
-    { "board_width",'w',POPT_ARG_INT,&board_width,0,"Checkerboard width","NUM" },
-    { "board_height",'h',POPT_ARG_INT,&board_height,0,"Checkerboard height","NUM" },
-    { "num_imgs",'n',POPT_ARG_INT,&num_imgs,0,"Number of checkerboard images","NUM" },
-    { "square_size",'s',POPT_ARG_FLOAT,&square_size,0,"Size of checkerboard square","NUM" },
-    { "imgs_directory",'d',POPT_ARG_STRING,&imgs_directory,0,"Directory containing images","STR" },
-    { "imgs_filename",'i',POPT_ARG_STRING,&imgs_filename,0,"Image filename","STR" },
-    { "extension",'e',POPT_ARG_STRING,&extension,0,"Image extension","STR" },
-    { "out_file",'o',POPT_ARG_STRING,&out_file,0,"Output calibration filename (YML)","STR" },
-    POPT_AUTOHELP
-    { NULL, 0, 0, NULL, 0, NULL, NULL }
-  };
+//    static struct poptOption options[] = {
+//            { "board_width",'w',POPT_ARG_INT,&board_width,0,"Checkerboard width","NUM" },
+//            { "board_height",'h',POPT_ARG_INT,&board_height,0,"Checkerboard height","NUM" },
+//            { "num_imgs",'n',POPT_ARG_INT,&num_imgs,0,"Number of checkerboard images","NUM" },
+//            { "square_size",'s',POPT_ARG_FLOAT,&square_size,0,"Size of checkerboard square","NUM" },
+//            { "imgs_directory",'d',POPT_ARG_STRING,&imgs_directory,0,"Directory containing images","STR" },
+//            { "imgs_filename",'i',POPT_ARG_STRING,&imgs_filename,0,"Image filename","STR" },
+//            { "extension",'e',POPT_ARG_STRING,&extension,0,"Image extension","STR" },
+//            { "out_file",'o',POPT_ARG_STRING,&out_file,0,"Output calibration filename (YML)","STR" },
+//            POPT_AUTOHELP
+//                    { NULL, 0, 0, NULL, 0, NULL, NULL }
+//    };
+//
+//    POpt popt(NULL, argc, argv, options, 0);
+//    int c;
+//    while((c = popt.getNextOpt()) >= 0) {}
 
-  POpt popt(NULL, argc, argv, options, 0);
-  int c;
-  while((c = popt.getNextOpt()) >= 0) {}
+    setup_calibration(board_width, board_height, square_size,
+                      imgs_directory, showChessboard);
 
-  setup_calibration(board_width, board_height, num_imgs, square_size,
-                   imgs_directory, imgs_filename, extension);
+    printf("Starting Calibration\n");
+    Mat K;
+    Mat D;
+    vector< Mat > rvecs, tvecs;
+    int flag = 0;
+    flag |= CV_CALIB_FIX_K4;
+    flag |= CV_CALIB_FIX_K5;
+    calibrateCamera(object_points, image_points, img.size(), K, D, rvecs, tvecs, flag);
 
-  printf("Starting Calibration\n");
-  Mat K;
-  Mat D;
-  vector< Mat > rvecs, tvecs;
-  int flag = 0;
-  flag |= CV_CALIB_FIX_K4;
-  flag |= CV_CALIB_FIX_K5;
-  calibrateCamera(object_points, image_points, img.size(), K, D, rvecs, tvecs, flag);
+    cout << "Calibration error: " << computeReprojectionErrors(object_points, image_points, rvecs, tvecs, K, D) << endl;
 
-  cout << "Calibration error: " << computeReprojectionErrors(object_points, image_points, rvecs, tvecs, K, D) << endl;
+    FileStorage fs(out_file, FileStorage::WRITE);
+    fs << "K" << K;
+    fs << "D" << D;
+    fs << "board_width" << board_width;
+    fs << "board_height" << board_height;
+    fs << "square_size" << square_size;
+    printf("Done Calibration\n");
 
-  FileStorage fs(out_file, FileStorage::WRITE);
-  fs << "K" << K;
-  fs << "D" << D;
-  fs << "board_width" << board_width;
-  fs << "board_height" << board_height;
-  fs << "square_size" << square_size;
-  printf("Done Calibration\n");
+    // Rectify an image
+    Mat img = imread("distord.ppm", CV_LOAD_IMAGE_COLOR);
+    cv::Mat imgU;
+    undistort(img, imgU, K, D);
+    imwrite("rect.ppm", imgU);
 
-  return 0;
+    return 0;
 }
